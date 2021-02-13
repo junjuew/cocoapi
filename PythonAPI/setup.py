b074648f972895c47da93cd16bdbacaa07c30a5a
fcakyon <fcakyon@gmail.com>
2021-02-13 22:14:15 +0300

    fix windows build

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -2,6 +2,9 @@ from setuptools import dist, setup, Extension
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
+# Note that the original compile flags below are GCC flags unsupported by
+# the Visual C++ 2015 build tools.
+# They can safely be removed.
 
 install_requires = [
     'setuptools>=18.0',
@@ -17,7 +20,8 @@ ext_modules = [
         'pycocotools._mask',
         sources=['./common/maskApi.c', 'pycocotools/_mask.pyx'],
         include_dirs=[np.get_include(), './common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        # extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=[],
     )
 ]
 
